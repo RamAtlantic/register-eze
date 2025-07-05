7883075793423453fa980d01f8d8ec376febf358
Ramiro Martin Arce <116601639+RamAtlantic@users.noreply.github.com>
2025-07-05 16:34:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,14 +129,6 @@ export default function RootLayout({
             `,
           }}
         />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: 'none' }}
-            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
-          />
-        </noscript>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <TrackingProvider>
             {children}
